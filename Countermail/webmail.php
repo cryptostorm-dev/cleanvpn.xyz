23acaf226c5dd91113734da5c571e22cad8932f2
Pattern Juggled ie ðørkßöt <pj@cryptostorm.is>
2015-04-01 09:17:43 -0400

    Create webmail.php
    view-source:https://countermail.com/webmail.php

---
+++ Countermail/webmail.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+    <head>
+<title>CounterMail - protecting your privacy</title>
+    <meta name="robots" content="noindex,nofollow" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link media="screen" rel="stylesheet" type="text/css" href="./templates/CMBlack_Green/css/options.css"  />
+<link media="screen" rel="stylesheet" type="text/css" href="./templates/CMBlack_Green/css/default.css"  />
+<link media="screen" rel="stylesheet" type="text/css" href="/src/style.php?"  />
+<link media="print" title="printerfriendly" rel="stylesheet" type="text/css" href="/css/print.css"  />
+<!--[if IE 6]>
+<style type="text/css">
+/* avoid stupid IE6 bug with frames and scrollbars */
+body {
+    height: expression(document.documentElement.clientHeight - 40);
+	 width: expression(document.documentElement.clientWidth - 40);
+}
+</style>
+<![endif]-->
+</head><script language='javascript' src='/cm.js?201412131'></script>
+<frameset id="framesid" rows="60,*" frameborder="0"  border="0" framespacing="0">
+<frame name="topframe" id="topframe" src="src/top.php?act=login&CMID=sk1ilot73m6535h5q79fbi7ie48c287a" scrolling=no  frameborder="0"  border="0" framespacing="0">
+<frameset framespacing='0' frameborder='0'  border='0' cols="196, *" id="fs1">
+<frame src="src/blank.php?CMID=sk1ilot73m6535h5q79fbi7ie48c287a" name="left" id="left" framespacing="0" frameborder="0" title="Folder List" />
+<frame src="src/login.php?CMID=sk1ilot73m6535h5q79fbi7ie48c287a" name="right" id="right"  framespacing="0" frameborder="0" title="Message List" />
+
+</frameset>
+</frameset>
+<noframes>Your web browser must have activated frames to use our service.</noframes></html>
